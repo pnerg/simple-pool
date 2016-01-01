1d76c74c6f389b571894b0ebcf0c75e086764fd8
Peter Nerg <peter.nerg@gmail.com>
2016-01-01 15:44:11 +0100

    #7 added more tests for markStaleInstances

--- src/test/java/simplepool/TestPoolQueueAbstract.java
+++ src/test/java/simplepool/TestPoolQueueAbstract.java
@@ -25,7 +25,6 @@ import org.junit.Test;
  */
 public abstract class TestPoolQueueAbstract extends BaseAssert {
 
-	
 	protected final PoolQueue<PoolableObject> queue;
 
 	TestPoolQueueAbstract(PoolQueue<PoolableObject> queue) {
@@ -50,16 +49,53 @@ public abstract class TestPoolQueueAbstract extends BaseAssert {
 	
 	@Test
 	public void markStaleInstances_emptyQueue() {
-		queue.markStaleInstances(Duration.ZERO, s -> {});
+		queue.markStaleInstances(Duration.ZERO, s -> s.destroy());
 	}
 
 	@Test
-	public void markStaleInstances_nonEmptyQueue() {
+	public void markStaleInstances_nonEmptyQueueAllValid() {
 		PoolableObject one = add("one");
-		queue.markStaleInstances(Duration.ofDays(1), s -> {});
+		queue.markStaleInstances(Duration.ofDays(1), s -> s.destroy());
+		
+		assertIsValid(one);
 		
 		//should still have the same instance in Queue
 		assertHead(one);
+		assertHeadIsEmpty();
+	}
+
+	@Test
+	public void markStaleInstances_nonEmptyQueueFoundStale() throws InterruptedException {
+		PoolableObject one = add("one");
+		PoolableObject two = add("two");
+		Thread.sleep(1); //sleep one to be sure we expire all instances
+		
+		//sweep with zero duration, all instances should be marked as stale
+		queue.markStaleInstances(Duration.ZERO, s -> s.destroy());
+		
+		assertIsDestroyed(one);
+		assertIsDestroyed(two);
+		
+		//queue should be empty
+		assertHeadIsEmpty();
+	}
+
+	@Test
+	public void markStaleInstances_withStaleInstancesInQueue() throws InterruptedException {
+		PoolableObject one = add("one");
+		PoolableObject two = add("two");
+		Thread.sleep(1); //sleep one to be sure we expire all instances
+		
+		//sweep with zero duration, all instances should be marked as stale
+		queue.markStaleInstances(Duration.ZERO, s -> s.destroy());
+		
+		assertIsDestroyed(one);
+		assertIsDestroyed(two);
+		
+		//sweep again, should make no difference as instances are already marked as dead
+		queue.markStaleInstances(Duration.ZERO, s -> s.destroy());
+		
+		//queue should be empty
 		assertHeadIsEmpty();
 	}
 	
@@ -77,8 +113,26 @@ public abstract class TestPoolQueueAbstract extends BaseAssert {
 		assertSomeEquals(expected, queue.head());
 	}
 	
+	/**
+	 * Asserts that the head of the queue is empty.
+	 */
 	void assertHeadIsEmpty() {
 		assertIsNone(queue.head());
 	}
 	
+	/**
+	 * Asserts that that the provided object has been destroyed due to going stale.
+	 * @param po
+	 */
+	void assertIsDestroyed(PoolableObject po) {
+		assertTrue("Expected ["+po+"] to be destroyed", po.isDestroyed());
+	}
+	
+	/**
+	 * Assert that the provided object is still valid, i.e. not destroyed.
+	 * @param po
+	 */
+	void assertIsValid(PoolableObject po) {
+		assertTrue("Expected ["+po+"] to be valid", po.isValid());
+	}
 }
